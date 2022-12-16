1f3633082108963b77e45f82f7f3fdb7cdb1ba7e
Alexxmfs <alexxmfs@hotmail.com>
2022-12-16 10:26:45 -0300

    building file add.jsx

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
-  return (
-    <div>
-      Add
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+
+    return (
+    <div classNames="form">
+        <h1>Add New Book</h1>
+        <input type="text" placeholder='title' />
+        <input type="text" placeholder='desc' />
+        <input type="number" placeholder='price' />
+        <input type="text" placeholder='cover' />
     </div>
   )
 }
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import { Link }  from "react-router-dom";
 
 const Books = () => {
   const [books, setBooks] = useState([])
@@ -6,15 +8,29 @@ const Books = () => {
   useEffect(() => {
     const fecthAllBooks = async ()=>{
         try{
-
+            const res = await axios.get("http://localhost:8800/books")
+            setBooks(res.data);
+            console.log(res);
         }catch(err){
             console.log(err);
         }
     }
+    fecthAllBooks()
   }, [])
     return (
     <div>
-      Books
+      <h1>Alex Shopping</h1>
+      <div className="books">
+        {books.map(book=>(
+            <div classNames="books" key={book.id}>
+                {book.cover && <img src={book.cover} alt="" />}
+                <h2>{book.title}</h2>
+                <p>{book.desc}</p>
+                <span>{book.price}</span>
+            </div>
+        ))}
+      </div>
+      <button><Link to="/add">Add new book</Link></button>
     </div>
   )
 }
