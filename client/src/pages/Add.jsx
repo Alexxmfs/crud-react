ccc1e3c62b44ee896873e5d3774ef7b0b2a9e260
Alexxmfs <alexxmfs@hotmail.com>
2022-12-19 13:27:10 -0300

    sucessful✔

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -10,51 +10,55 @@ const Add = () => {
     cover: "",
   });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  const handleClick = async e => {
-    e.preventDefault()
-    try{
-      await axios.post("http://localhost:8800/books", book)
-      navigate("/")
-    }catch(err){
-      console.log(err)
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/books", book);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   console.log(book);
   return (
-    <div className="form">
-      <h1>Add New Book</h1>
-      <input
-        type="text"
-        placeholder="title"
-        onChange={handleChange}
-        name="title"
-      />
-      <input
-        type="text"
-        placeholder="desc"
-        onChange={handleChange}
-        name="desc"
-      />
-      <input
-        type="number"
-        placeholder="price"
-        onChange={handleChange}
-        name="price"
-      />
-      <input
-        type="text"
-        placeholder="cover"
-        onChange={handleChange}
-        name="cover"
-      />
-      <button className="formButton" onClick={handleClick}>Add</button>
+    <div className="container">
+      <div className="form">
+        <h1>Add New Book</h1>
+        <input
+          type="text"
+          placeholder="Digite um titulo"
+          onChange={handleChange}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="Digite uma descrição"
+          onChange={handleChange}
+          name="desc"
+        />
+        <input
+          type="number"
+          placeholder="Digite um preço"
+          onChange={handleChange}
+          name="price"
+        />
+        <input
+          type="text"
+          placeholder="URL image"
+          onChange={handleChange}
+          name="cover"
+        />
+        <button className="formButton" onClick={handleClick}>
+          Add
+        </button>
+      </div>
     </div>
   );
 };
--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -10,54 +10,58 @@ const Update = () => {
     cover: "",
   });
 
-  const navigate = useNavigate()
-  const location = useLocation()
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const bookId = location.pathname.split("/")[2]
+  const bookId = location.pathname.split("/")[2];
 
   const handleChange = (e) => {
     setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  const handleClick = async e => {
-    e.preventDefault()
-    try{
-      await axios.put("http://localhost:8800/books/" + bookId, book)
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put("http://localhost:8800/books/" + bookId, book);
       navigate("/");
-    }catch(err){
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   console.log(book);
   return (
-    <div className="form">
-      <h1>Update the Book</h1>
-      <input
-        type="text"
-        placeholder="title"
-        onChange={handleChange}
-        name="title"
-      />
-      <input
-        type="text"
-        placeholder="desc"
-        onChange={handleChange}
-        name="desc"
-      />
-      <input
-        type="number"
-        placeholder="price"
-        onChange={handleChange}
-        name="price"
-      />
-      <input
-        type="text"
-        placeholder="cover"
-        onChange={handleChange}
-        name="cover"
-      />
-      <button className="formButton" onClick={handleClick}>Update</button>
+    <div className="container">
+      <div className="form">
+        <h1>Update the Book</h1>
+        <input
+          type="text"
+          placeholder="Digite um titulo"
+          onChange={handleChange}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="Digite uma descrição"
+          onChange={handleChange}
+          name="desc"
+        />
+        <input
+          type="number"
+          placeholder="Digite um preço"
+          onChange={handleChange}
+          name="price"
+        />
+        <input
+          type="text"
+          placeholder="URL image"
+          onChange={handleChange}
+          name="cover"
+        />
+        <button className="formButton" onClick={handleClick}>
+          Update
+        </button>
+      </div>
     </div>
   );
 };
